ad3e3e17f02176cc2fd79997be3a98aee8f556d8
antigones <juna.salviati@gmail.com>
2023-02-12 18:31:36 +0100

    turmites with rules

--- turmites/main.py
+++ turmites/main.py
@@ -3,12 +3,13 @@ from PIL import Image
 import imageio
 from turmite import Turmite
 
+# 120080 Langton's ant
 
-turmite = Turmite(size=200, n_iterations=10000)
+turmite = Turmite(size=200, rule='181181121010', n_iterations=10000)
 steps = turmite.run()
 
-imgs = [Image.fromarray(img.astype(np.uint8)) for img in steps]
+imgs = [Image.fromarray(np.array(img)) for img in steps]
 # duration is the number of milliseconds between frames; this is 40 frames per second
 
-#imageio.mimsave('new.gif', imgs, duration=1/100000)
+imageio.mimsave('new.gif', imgs)
 imageio.mimsave('new1.gif', imgs[8342:])
--- turmites/turmite.py
+++ turmites/turmite.py
@@ -4,12 +4,28 @@ from copy import deepcopy
 
 class Turmite:
 
-    def __init__(self, size, n_iterations=1000):
+    def __init__(self, size, rule, n_iterations=1000 ):
+        """
+                                current-color = 0    current-color = 1
+            current-state = 0              1,8,1                1,8,1
+            current-state = 1              1,2,1                0,1,0
+
+            The {1,8,1} triple therefore states that the colour should change to 1, 
+            turn left (8) and adopt state 1 before moving forwards.
+
+            The direction to turn is specified by:
+
+            1: no turn
+            2: right
+            4: u-turn
+            8: left
+        """
+
         self.size = size
         self.n_iterations = n_iterations
-        self.grid = np.zeros((self.size, self.size))
+        self.grid = [[0 for _ in range(self.size)] for _ in range(self.size)]
         self.starting_point = (self.size // 2, self.size // 2)
-        self.grid[self.starting_point] = 0
+        self.grid[self.starting_point[0]][self.starting_point[1]] = 0
         
         self.state = 0
     
@@ -21,33 +37,43 @@ class Turmite:
             (1,  0),   # s
             (0, -1),   # w
             ]
+
+        """
+            how much to advance in neighbors_idx to make that move
+        """
         
+        self.move_kind_to_idx = {
+                                    1: 0,
+                                    2: 1,
+                                    4: 2,
+                                    8: -1
+                                }
+                            
+        self.rule = rule
+        self.states_and_rules = self.parse_rule()
         self.steps = []
+
+    def parse_rule(self):
+        
+        # 120080
+        # 120 080
+        # 181181121010
+        # 181 181 121 010
+        parts = [self.rule[i:i+3] for i in range(0, len(self.rule), 3)]
+        states_and_rules = [parts[i:i+2] for i in range(0, len(parts), 2)]
+        return states_and_rules
 
 
     def run(self):
      
         for _ in range(self.n_iterations):
-            current_color = self.grid[self.starting_point]
-
-            if current_color == 0:
-                if self.state == 0:
-                    self.grid[self.starting_point] = 255
-                    self.heading_idx = (self.heading_idx + 1) % 4
-                if self.state == 1:
-                    self.grid[self.starting_point] = 0
-                self.state = 0         
-
-            if current_color == 255:
-                if self.state == 0:
-                    self.grid[self.starting_point] = 255
-                    self.heading_idx = (self.heading_idx + 1) % 4
-                if self.state == 1:
-                    self.grid[self.starting_point] = 0 
-                self.state = 1
-
-            # step, updating starting point toward position
+            current_color = self.grid[self.starting_point[0]][self.starting_point[1]]
+            write_color, move_kind, new_state = self.states_and_rules[self.state][current_color]
+            self.grid[self.starting_point[0]][self.starting_point[1]] = int(write_color)
+            self.heading_idx = (self.heading_idx + self.move_kind_to_idx[int(move_kind)]) % 4
+            self.state = int(new_state)
             self.starting_point = (self.starting_point[0] + self.neighbors_idxs[self.heading_idx][0], 
                                     self.starting_point[1] + self.neighbors_idxs[self.heading_idx][1])
+
             self.steps.append(deepcopy(self.grid))
         return self.steps
